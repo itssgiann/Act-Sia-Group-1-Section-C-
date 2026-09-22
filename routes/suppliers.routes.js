8b1f9ad76d026d136e1b57545462b848def9891c
Dev-Hisayuki <allenjay.alcorcon@gmail.com>
2026-09-22 18:41:22 +0800

    feat(suppliers): add Get route to find suppliers by id

--- routes/suppliers.routes.js
+++ routes/suppliers.routes.js
@@ -10,32 +10,3 @@ router.get('/', (req, res) => {
     res.json(suppliers);
 });
 
-router.get('/id:', (req, res) => {
-    const supplier = suppliers.find(s => s.id === parseInt(req.params.id));
-    if (!supplier) {
-        return res.status(404).json({ message: "Supplier not found." });
-    }
-
-    res.json(supplier);
-});
-
-router.post('/', (req, res) => {
-    const companyName = req.body.companyName;
-    const country = req.body.country;
-
-    if(!companyName || !country) {
-        return res.status(404).json({ message: "Missing required fields." })
-    }
-
-    const newSupplier = {
-        id: suppliers.lenght = 1,
-        companyName: companyName,
-        country: country
-    };
-
-    suppliers.push(newSupplier);
-
-        res.status(201).json(newSupplier);
-});
-
-module.exports = router;
